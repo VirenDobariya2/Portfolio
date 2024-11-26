c7a766578362c757f3492a61209bd62d5a224a2c
VirenDobariya2 <virendobariya4@gmail.com>
2024-11-26 12:10:42 +0530

    profile my css add

---
+++ src/components/Skills/SkillPage/Skill.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "../SkillPage/Skill.css";
+import { SKILLS } from "../../../utils/data.js";
+import SkillsInfoCard from "../SkillsInfoCard/SkillsInfoCard.jsx";
+import SkillCardone from "../SkillCard/SkillCardone.jsx";
+
+
+const Skill = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
+  return (
+    <section className="skills-container">
+      <h5>Technical Proficiency</h5>
+
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCardone
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectSkill(item)
+              }}
+            />
+          ))}
+        </div>
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skill;
